65a9ab5ca1f393169ce8ba778ec523594c154a49
JoãoAndreotti90 <joaogilberto238@gmail.com>
2025-11-03 13:08:46 -0300

    finalizado versão mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
 import { NavLink } from "react-router-dom";
-import { useAuthentication } from "../hooks/useAuthentication"; 
-import { useAuthValue } from "../contexts/AuthContext"; 
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../contexts/AuthContext";
 import styles from "./Navbar.module.css";
+
+import { useState } from "react";
 
 const Navbar = () => {
   const { logout } = useAuthentication();
   const { user } = useAuthValue();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <NavLink className={styles.brand} to="/">
+      <NavLink className={styles.brand} to="/" onClick={closeMenu}>
         Mini <span>Blog</span>
       </NavLink>
-      <ul className={styles.links_list}>
-        <li>
+
+      <button
+        className={styles.hamburger}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        &#9776;
+      </button>
+
+      <ul
+        className={`${styles.links_list} ${isMenuOpen ? styles.open : ""}`}
+      >
+        <li onClick={closeMenu}>
           <NavLink
             to="/"
             className={({ isActive }) => (isActive ? styles.active : "")}
@@ -23,7 +40,7 @@ const Navbar = () => {
         </li>
         {!user && (
           <>
-            <li>
+            <li onClick={closeMenu}>
               <NavLink
                 to="/login"
                 className={({ isActive }) => (isActive ? styles.active : "")}
@@ -31,7 +48,7 @@ const Navbar = () => {
                 Entrar
               </NavLink>
             </li>
-            <li>
+            <li onClick={closeMenu}>
               <NavLink
                 to="/register"
                 className={({ isActive }) => (isActive ? styles.active : "")}
@@ -43,7 +60,7 @@ const Navbar = () => {
         )}
         {user && (
           <>
-            <li>
+            <li onClick={closeMenu}>
               <NavLink
                 to="/posts/create"
                 className={({ isActive }) => (isActive ? styles.active : "")}
@@ -51,7 +68,7 @@ const Navbar = () => {
                 Novo post
               </NavLink>
             </li>
-            <li> 
+            <li onClick={closeMenu}>
               <NavLink
                 to="/dashboard"
                 className={({ isActive }) => (isActive ? styles.active : "")}
@@ -61,7 +78,7 @@ const Navbar = () => {
             </li>
           </>
         )}
-        <li>
+        <li onClick={closeMenu}>
           <NavLink
             to="/about"
             className={({ isActive }) => (isActive ? styles.active : "")}
